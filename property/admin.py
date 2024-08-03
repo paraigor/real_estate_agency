f19191335311545beffb90a96d47dea5252c4fb7
paraigor <103633539+paraigor@users.noreply.github.com>
2024-08-03 21:43:39 +0300

    Update admin UI for Flat and Owner models

--- property/admin.py
+++ property/admin.py
@@ -3,20 +3,30 @@ from django.contrib import admin
 from .models import Complaint, Flat, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Owner.flat.through
+    raw_id_fields = ["owner", "flat",]
+    extra = 0
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ["id",]
     readonly_fields = ["created_at",]
     list_display = ["id", "town", "address"]
     list_filter = ["new_building", "rooms_number", "floor", "has_balcony",]
     raw_id_fields = ["liked_by",]
+    inlines = [OwnerInline]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ["flat",]
 
+
 class OwnerAdmin(admin.ModelAdmin):
     search_fields = ["full_name",]
-    raw_id_fields = ["flat",]
+    inlines = [OwnerInline]
+    exclude = ["flat"]
+
 
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
